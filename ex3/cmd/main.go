34213a26d8fac6cca1773fa5d09a0f14ad817b32
Valdemar Ceccon <valdemar.ceccon@gmail.com>
2018-07-30 23:51:39 -0300

    ex3 solved

--- ex3/cmd/main.go
+++ ex3/cmd/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/valdemarceccon/gophercises/ex3/ownstory"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "The port to start CYOS server")
 	filePath := flag.String("file", "gopher.json", "File path to story JSON.")
 	file, err := os.Open(*filePath)
 
@@ -24,5 +27,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(story)
+	handler := ownstory.NewHandler(story)
+	fmt.Printf("Starting server at: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), handler))
 }
