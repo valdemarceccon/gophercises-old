18d6d21314b5a1f87e57cd73e0826f7106326411
Valdemar Ceccon <valdemar.ceccon@gmail.com>
2019-04-01 01:54:12 -0300

    finished "making choises" section

--- ex18/main.go
+++ ex18/main.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"text/template"
 
 	"github.com/valdemarceccon/gophercises/ex18/primitive"
@@ -28,54 +29,71 @@ func main() {
 		fmt.Fprintf(w, html)
 	})
 
+	mux.HandleFunc("/modify/", func(w http.ResponseWriter, r *http.Request) {
+		f, err := os.Open("./img/" + filepath.Base(r.URL.Path))
+		defer f.Close()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		ext := filepath.Ext(f.Name())[1:]
+
+		modeStr := r.FormValue("mode")
+
+		if modeStr == "" {
+			renderModeChoices(w, r, f, ext)
+			return
+		}
+
+		mode, err := strconv.Atoi(modeStr)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		nStr := r.FormValue("n")
+
+		if nStr == "" {
+			renderNumShapeChoices(w, r, f, ext, primitive.Mode(mode))
+			return
+		}
+
+		_, err = strconv.Atoi(nStr)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		http.Redirect(w, r, "/img/"+filepath.Base(f.Name()), http.StatusFound)
+
+	})
+
 	mux.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
 		file, header, err := r.FormFile("image")
 
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
-			fmt.Println(err)
 			return
 		}
 		defer file.Close()
 
 		ext := filepath.Ext(header.Filename)[1:]
-
-		a, err := genImage(file, ext, 33, primitive.ModeCircle)
+		onDisk, err := tempfile("", ext)
+		defer onDisk.Close()
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			panic(err)
+			http.Error(w, "Something went wrong", http.StatusInternalServerError)
+			return
 		}
-		file.Seek(0, 0)
+		_, err = io.Copy(onDisk, file)
 
-		b, err := genImage(file, ext, 33, primitive.ModeEllipse)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			panic(err)
+			http.Error(w, "Something went wrong", http.StatusInternalServerError)
+			return
 		}
-		file.Seek(0, 0)
-		c, err := genImage(file, ext, 33, primitive.ModePolygon)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			panic(err)
-		}
-		file.Seek(0, 0)
-		d, err := genImage(file, ext, 33, primitive.ModeCombo)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			panic(err)
-		}
-		file.Seek(0, 0)
 
-		html := `<html><body>
-		{{range .}}
-			<img src="/{{.}}">
-		{{end}}
-		</body></html>`
-
-		tpl := template.Must(template.New("").Parse(html))
-		images := []string{a, b, c, d}
-
-		tpl.Execute(w, images)
+		http.Redirect(w, r, "/modify/"+filepath.Base(onDisk.Name()), http.StatusFound)
 	})
 
 	fs := http.FileServer(http.Dir("./img/"))
@@ -83,6 +101,105 @@ func main() {
 	mux.Handle("/img/", http.StripPrefix("/img/", fs))
 
 	log.Fatal(http.ListenAndServe(":3000", mux))
+}
+
+type genOpts struct {
+	N int
+	M primitive.Mode
+}
+
+func renderNumShapeChoices(w http.ResponseWriter, r *http.Request, rs io.ReadSeeker, ext string, mode primitive.Mode) {
+	opts := []genOpts{
+		{N: 50, M: mode},
+		{N: 100, M: mode},
+		{N: 150, M: mode},
+		{N: 200, M: mode},
+	}
+
+	imgs, err := genImages(rs, ext, opts...)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		panic(err)
+	}
+	html := `<html><body>
+	{{range .}}
+		<a href="/modify/{{.Name}}?mode={{.Mode}}&n={{.NumShapes}}">
+			<img style="width:20%;" src="/img/{{.Name}}">
+		</a>
+	{{end}}
+	</body></html>`
+
+	tpl := template.Must(template.New("").Parse(html))
+	type dataStruct struct {
+		Name      string
+		Mode      primitive.Mode
+		NumShapes int
+	}
+	var data []dataStruct
+	for i, img := range imgs {
+		data = append(data, dataStruct{
+			Name:      filepath.Base(img),
+			Mode:      opts[i].M,
+			NumShapes: opts[i].N,
+		})
+	}
+
+	tpl.Execute(w, data)
+}
+
+func genImages(rs io.ReadSeeker, ext string, opts ...genOpts) ([]string, error) {
+	var ret []string
+	for _, opt := range opts {
+		rs.Seek(0, 0)
+		f, err := genImage(rs, ext, opt.N, opt.M)
+
+		if err != nil {
+			return nil, err
+		}
+
+		ret = append(ret, f)
+	}
+
+	return ret, nil
+}
+
+func renderModeChoices(w http.ResponseWriter, r *http.Request, rs io.ReadSeeker, ext string) {
+	opts := []genOpts{
+		{N: 10, M: primitive.ModeCircle},
+		{N: 10, M: primitive.ModeBeziers},
+		{N: 10, M: primitive.ModePolygon},
+		{N: 10, M: primitive.ModeCombo},
+	}
+
+	imgs, err := genImages(rs, ext, opts...)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		panic(err)
+	}
+	html := `<html><body>
+	{{range .}}
+		<a href="/modify/{{.Name}}?mode={{.Mode}}">
+			<img style="width:20%;" src="/img/{{.Name}}">
+		</a>
+	{{end}}
+	</body></html>`
+
+	tpl := template.Must(template.New("").Parse(html))
+	type dataStruct struct {
+		Name string
+		Mode primitive.Mode
+	}
+	var data []dataStruct
+	for i, img := range imgs {
+		data = append(data, dataStruct{
+			Name: filepath.Base(img),
+			Mode: opts[i].M,
+		})
+	}
+
+	tpl.Execute(w, data)
 }
 
 func genImage(r io.Reader, ext string, numShapes int, mode primitive.Mode) (string, error) {
